762ab83074f65d316f59d873268319b3f2f81d72
joão mendes porth <joao.m.porth@gmail.com>
2023-09-13 00:05:14 -0300

    comprovantes

--- main.py
+++ main.py
@@ -6,12 +6,123 @@ import requests
 from anticaptchaofficial.hcaptchaproxyless import hCaptchaProxyless
 
 from flask import Flask, jsonify, request
+from PIL import Image, ImageDraw, ImageFont
 
 
 def clean(text) -> str:
     return str(re.sub(r'[^\w\s]', '', str(text))).replace(' ','').strip()
 
 API_KEY = ''
+
+def certificado_nao_consta(cpf, ano):
+
+    imagem = Image.open('nao_consta.png')
+
+    desenhar = ImageDraw.Draw(imagem)
+    fonte = ImageFont.truetype('fonte.ttf', 18)
+
+    posicao = (245, 145)
+    cor = (120, 120, 120)
+
+    desenhar.text(posicao, cpf, fill=cor, font=fonte)
+
+    imagem.save(os.path.join('comprovantes', f'{cpf}_{ano}.png'))
+
+    imagem.close()
+
+
+
+def certificado_a_restituir(cpf, nome, ano, banco, agencia, lote, data_disponibilidade, situacao):
+
+    imagem = Image.open('a_restituir.png')
+
+    desenhar = ImageDraw.Draw(imagem)
+
+    #cpf
+    fonte = ImageFont.truetype('fonte.ttf', 16)
+    posicao = (20, 70)
+    cor = (255, 255, 255)
+    desenhar.text(posicao, cpf, fill=cor, font=fonte)
+
+    # nome
+    fonte = ImageFont.truetype('fonte.ttf', 12)
+    posicao = (20, 95)
+    cor = (180, 180, 180)
+    desenhar.text(posicao, nome, fill=cor, font=fonte)
+
+    # ano
+    fonte = ImageFont.truetype('fonte.ttf', 12)
+    posicao = (35, 160)
+    cor = (255, 255, 255)
+    desenhar.text(posicao, ano, fill=cor, font=fonte)
+
+    # banco
+    fonte = ImageFont.truetype('fonte.ttf', 12)
+    posicao = (20, 270)
+    cor = (255, 255, 255)
+    desenhar.text(posicao, banco, fill=cor, font=fonte)
+
+
+    # agencia
+    fonte = ImageFont.truetype('fonte.ttf', 15)
+    posicao = (400, 270)
+    cor = (255, 255, 255)
+    desenhar.text(posicao, agencia, fill=cor, font=fonte)
+
+
+    # lote
+    fonte = ImageFont.truetype('fonte.ttf', 12)
+    posicao = (20, 335)
+    cor = (255, 255, 255)
+    desenhar.text(posicao, lote, fill=cor, font=fonte)
+
+
+    # data_disponibilidade
+    fonte = ImageFont.truetype('fonte.ttf', 12)
+    posicao = (400, 335)
+    cor = (255, 255, 255)
+    desenhar.text(posicao, data_disponibilidade, fill=cor, font=fonte)
+
+    # situacao
+    fonte = ImageFont.truetype('fonte.ttf', 12)
+    posicao = (20, 360)
+    cor = (255, 255, 255)
+    desenhar.text(posicao, situacao, fill=cor, font=fonte)
+
+
+    imagem.save(os.path.join('comprovantes', f'{cpf}_{ano}.png'))
+
+    imagem.close()
+
+
+def certificado_sem_saldo(cpf, nome, ano):
+
+    imagem = Image.open('sem_saldo.png')
+
+    desenhar = ImageDraw.Draw(imagem)
+
+    #cpf
+    fonte = ImageFont.truetype('fonte.ttf', 16)
+    posicao = (20, 70)
+    cor = (255, 255, 255)
+    desenhar.text(posicao, cpf, fill=cor, font=fonte)
+
+    # nome
+    fonte = ImageFont.truetype('fonte.ttf', 12)
+    posicao = (20, 95)
+    cor = (180, 180, 180)
+    desenhar.text(posicao, nome, fill=cor, font=fonte)
+
+    # ano
+    fonte = ImageFont.truetype('fonte.ttf', 12)
+    posicao = (35, 160)
+    cor = (255, 255, 255)
+    desenhar.text(posicao, ano, fill=cor, font=fonte)
+
+    imagem.save(os.path.join('comprovantes', f'{cpf}_{ano}.png'))
+
+    imagem.close()
+
 
 
 def h_captcha():
@@ -120,10 +231,29 @@ def get_json():
 
     print(response)
 
+    cpf = clean(cpf)
+
+    cpf = f"{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}"
+
+    if response['status']:
+        # não consta
+        if 'Sua declara&ccedil;&atilde;o n&atilde;o consta na base de dados da Receita Federal.' in response['message']['situacao']:
+            certificado_nao_consta(cpf, ano)
+
+        if 'Saldo inexistente de imposto a pagar ou a restituir.' in response['message']['situacao']:
+            certificado_sem_saldo(cpf, response['message']['nomeContribuinte'], ano)
+
+
+        if 'Os dados da liberação de sua restituição estão descritos abaixo:' in response['message']['situacao']:
+            certificado_a_restituir(cpf, response['message']['nomeContribuinte'], ano, response['message']['dadosBancarios']['banco'], response['message']['dadosBancarios']['agencia'], response['message']['restituicao']['lote'], response['message']['restituicao']['dataDisponibilidade'], response['message']['restituicao']['situacaoRestituicao'])
+
+
     return jsonify(response)
 
 if __name__ == '__main__':
+    pass
     app.run(debug=True)
 
     # /api?cpf=012.392.742-03&data_nascimento=03/01/1992&ano=2020
 
+    #certificado_processada('101.863.589-03', 'JOAO VITOR MENDES PORTH', '2022')
